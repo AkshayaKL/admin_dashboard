390230f5881e2ea32c1d47afde8bf4ab2fe90c0a
AkshayaKL <achu4008@yahoo.com>
2021-07-28 12:04:09 +0530

    pages modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-
-import Dashboard from './components/admindashboard.component';
-import './App.css';
-
-
-
+import Dashboard from "./components/admindashboard.component";
+import "./App.css";
 
 export function App() {
-
-  
   return (
-    
-    <div id = "dashboard" style={{'height':'100%'}}>
- 
-    <Dashboard style={{'height':'100%', }}/>
+    <div id="dashboard" style={{ height: "100%" }}>
+      <Dashboard style={{ height: "100%" }} />
     </div>
-   
   );
 }
-
-
--- src/components/admindashboard.component.js
+++ src/components/admindashboard.component.js
@@ -3,7 +3,8 @@ import { DataGrid } from "@material-ui/data-grid";
 import DeleteIcon from "@material-ui/icons/Delete";
 import EditIcon from "@material-ui/icons/Edit";
 import { MenuItem, Select, TextField } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 import { Button } from "@material-ui/core";
 import "./adminboard.component.css";
 import { getUserData } from "../services/usersFetch";
@@ -23,10 +24,11 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
   },
 }));
+
+
 export default function Dashboard() {
   const classes = useStyles();
 
- 
   const gridStyle = {
     display: "flex",
     height: 580,
@@ -34,7 +36,6 @@ export default function Dashboard() {
   };
   let unique = [];
 
- 
   const [page, setPage] = useState(0);
   const [users, setUsers] = useState([]);
   const [searchValue, setSearchValue] = useState("");
@@ -43,15 +44,16 @@ export default function Dashboard() {
   const [openEdit, setopenEdit] = useState(false);
   const [editedRow, setEditedRow] = useState({});
   let pages = [];
-
-
-  useEffect(()=>{
-    getUserData().then((userdata)=>{
+  
+  useEffect(() => {
+   
+  
+      getUserData().then((userdata) => {
       console.log(userdata.data);
       setUsers(userdata.data);
-      setReturnedUsers(userdata.data)
+      setReturnedUsers(userdata.data);
     });
-  },[])
+  },[]);
   var handleClose = () => {
     console.log(editedRow);
     console.log([
@@ -86,6 +88,48 @@ export default function Dashboard() {
     setSelectionModel([]);
   };
 
+  function Pages()
+    {
+      return(
+      returnedUsers.map((pageno, index) => {
+      if (pages.indexOf(Math.floor(index / 10) + 1) === -1) {
+        pages.push(Math.floor(index / 10) + 1);
+
+        
+          <Button
+            onClick={() => {
+              setSelectionModel([]);
+              setPage(Math.floor(index / 10));
+            }}
+            size="small"
+            id="page"
+          >
+            {Math.floor(index / 10) + 1}
+          </Button>
+        
+          }
+      
+    }))}
+  
+
+  var customPagination =()=>{return(<div id="pages">
+        <Button
+         onClick={() => {
+          setSelectionModel([]);
+          setPage(0);
+        }}
+         id="page" size="small"><DoubleArrowIcon id="firstPage"/>
+         </Button>
+          
+          <Pages/>
+        
+           <Button
+            onClick={() => {
+              setSelectionModel([]);
+              setPage(Math.floor(returnedUsers.length/10));
+            }}
+            id="page" size="small"><DoubleArrowIcon/></Button>
+        </div>)}
   var editRows = (rowToBeEdited) => {
     setEditedRow(rowToBeEdited.row);
     setopenEdit(true);
@@ -111,7 +155,7 @@ export default function Dashboard() {
   };
 
   const columns = [
-    { field: "id", headerName: "ID", flex: 0.2, sortable: false },
+    { field: "id", headerName: "ID", flex: 0.2, sortable: false, },
     { field: "name", headerName: "Name", flex: 1, sortable: false },
     { field: "email", headerName: "Email", flex: 1, sortable: false },
     { field: "role", headerName: "Role", flex: 0.2, sortable: false },
@@ -148,7 +192,6 @@ export default function Dashboard() {
   ];
   return (
     <div style={gridStyle} className={classes.root}>
-   
       <Dialog
         onClose={handleClose}
         aria-labelledby="simple-dialog-title"
@@ -205,36 +248,34 @@ export default function Dashboard() {
           </Button>
         </DialogActions>
       </Dialog>
-      <div style={{ flexgrow: 1, flexBasis: "90%"}}>
+      <div style={{ flexgrow: 1, flexBasis: "90%" }}>
         <div id="search">
-          
-          <div id="searchField" ><TextField
-            id="searchField" 
-            
-            autoFocus
-            margin="dense"
-           
-            type="name"
-            
-            placeholder ={"Search for users.."}
-            value={searchValue}
-            onChange={(e) => {
-              handleSearch(e);
-            }}
-          />
-
-       
+          <div id="searchField">
+            <TextField
+              id="searchField"
+              autoFocus
+              margin="dense"
+              type="name"
+              placeholder={"Search for users.."}
+              value={searchValue}
+              onChange={(e) => {
+                handleSearch(e);
+              }}
+            />
           </div>
-         
         </div>
 
         <DataGrid
           disableColumnFilter
           rowHeight={37}
           pageSize={10}
+          components={{
+            Pagination: customPagination
+          }}
           rows={returnedUsers}
           columns={columns}
           checkboxSelection
+          nextIconButtonProps={{visibility:'hidden'}}
           page={page}
           disableSelectionOnClick
           onPageChange={(params) => {
@@ -253,57 +294,27 @@ export default function Dashboard() {
             console.log(newSelection);
           }}
         />
-     
-
-
-
-
       </div>
-      
-     <div id="pagination" >
 
-     <Button id="deleteBulk"
-        style={{
-          visibility: selectionModel.length > 1 ? "visible" : "hidden",
-          backgroundColor: "pink",
-          border: "1.5px solid red",
-        }}
-        onClick={() => {
-          deleteRows();
-        }}
-        size="small"
-        disabled={selectionModel.length > 1 ? false : true}
-      >
-        Delete {selectionModel.length} rows
-        <DeleteIcon style={{ color: "red" }} />
-      </Button>
-        <div id="pages">{
-       
-          returnedUsers.map((pageno, index)=>{
-          if (pages.indexOf(Math.floor(index / 10) + 1) === -1) {
-            pages.push(Math.floor(index / 10) + 1);
-
-            return (
-              <Button
-                
-                onClick={() => {
-                  setSelectionModel([]);
-                  setPage(Math.floor(index / 10));
-                }}
-                size="small"
-                id="page"
-                
-              >
-                {Math.floor(index / 10) + 1}
-              </Button>
-            );
-          }
-         
-        })}
-      
-   </div>
-   
-    </div>
+      <div id="pagination">
+        <Button
+          id="deleteBulk"
+          style={{
+            visibility: selectionModel.length > 1 ? "visible" : "hidden",
+            backgroundColor: "pink",
+            border: "1.5px solid red",
+          }}
+          onClick={() => {
+            deleteRows();
+          }}
+          size="small"
+          disabled={selectionModel.length > 1 ? false : true}
+        >
+          Delete {selectionModel.length} rows
+          <DeleteIcon style={{  transform: "rotate(180deg)" }} />
+        </Button>
+        
+      </div>
     </div>
   );
 }
